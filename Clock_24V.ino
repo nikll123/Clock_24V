283d0a5c44c2a89c021dbcac654fd5a6cb87e5ba
KirSanHP <DieselHP@mail.ru>
2020-01-17 22:59:45 +0300

    Update Clock_24V.ino
    A bit optimization

--- Clock_24V.ino
+++ Clock_24V.ino
@@ -10,6 +10,7 @@
 #define HOUR_ADD 0  //1 byte
 #define MIN_ADD 1   //1 byte
 
+#define MIN_CLOCK_VOLTAGE 19    //Minimum voltage required for stable clock work
 
 //значения клавиш
 #define NONE 0
@@ -47,7 +48,9 @@ void setup() {
 
   pinMode (out1pin, OUTPUT);
   pinMode (out2pin, OUTPUT);
-  pinMode (BTN_PIN, INPUT);
+  pinMode (BTN1_PIN, INPUT);
+  pinMode (BTN2_PIN, INPUT);
+  pinMode (BTN3_PIN, INPUT);
   pinMode (VOLT_PIN, INPUT);
   pinMode (LED_BUILTIN, OUTPUT);
   if (EEPROM.get (HOUR_ADD, clock_h) == 255) {
@@ -63,24 +66,14 @@ void setup() {
 }
 
 void loop() {
-  if (millis() >= millisPrev) {   //каждую секунду прибавляем по секунде, а так же пересчитываем это в минуты и часы
+  if (millis() >= millisPrev) {
     millisPrev += 1000;
-    time.seconds ++;
-    if (time.seconds == 60) {
-      time.seconds = 0;
-      time.minutes ++;
-      if (time.minutes == 60) {
-        time.minutes = 0;
-        time.hours ++;
-        if (time.hours == 12)
-          time.hours = 0;
-      }
-    }
+    time.gettime();
   }
 
-  if (map ( (analogRead (VOLT_PIN) / 4), 0, 255, 0, 24) > 19) {
-    if  (time.hours != clock_h || time.minutes != clock_m)  {
-    digitalWrite (LED_BUILTIN, LOW);
+  if (checkPowerVoltage(VOLT_PIN) ) {
+    if (timeNotMatch() ) {
+      digitalWrite (LED_BUILTIN, LOW);
       clock_m ++;
       if (clock_m == 60) {
         clock_m = 0;
@@ -95,15 +88,15 @@ void loop() {
   }
   else
     digitalWrite (LED_BUILTIN, HIGH);
-  }
+}
 
 void clockSwitch (byte minutes) {
   byte _pin;
-  if (minutes & 1) 
+  if (minutes % 2 == 0)
     _pin = out1pin;
   else
     _pin = out2pin;
-    
+
   digitalWrite (_pin, HIGH);
   digitalWrite (LED_BUILTIN, HIGH);
   delay (100);
@@ -112,8 +105,13 @@ void clockSwitch (byte minutes) {
   delay (100);
 }
 
-void formatTime () {
-  
+bool timeNotMatch() {
+  return (time.hours != clock_h || time.minutes != clock_m);
+}
+
+bool checkPowerVoltage(byte _pin) {
+  byte voltage = (map ( (analogRead (_pin) / 4), 0, 255, 0, 24) );
+  return (voltage > MIN_CLOCK_VOLTAGE);
 }
 
 void I2C_lcdStart() {
